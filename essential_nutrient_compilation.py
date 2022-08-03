b53a0486c1db7ea42949c23e0b7f7c70eb9f18e2
jmol0917 <31610027+jmol0917@users.noreply.github.com>
2022-08-03 18:07:49 +1000

    Update and rename strain_compilation.py to essential_nutrient_compilation.py

--- essential_nutrient_compilation.py
+++ essential_nutrient_compilation.py
@@ -4,8 +4,8 @@ on 30/04/2020
 """
 
 '''
-Compiles .csv files generated in combinatorial_analysis.py (script 1) from individual microbes into single .csv
-tables. Then generates charts and other relevant files for further analysis.
+Compiles .csv files generated in essential_nutrient_assessment.py from individual microbes into a single .csv
+tables. The new file is saved in the same folder were individual files are read from.
 '''
 import pandas as pd
 import seaborn as sns
@@ -20,22 +20,20 @@ from scipy.stats import spearmanr
 from sklearn.linear_model import LinearRegression
 
 # Input (directory where individual strain files are saved) and Output directory (where Boolean table will be saved)
-path_out = ''
+path = ''
 
 # makes a list of models/microbes that are in the consumption folder (models generated in script 1)
 # this avoids any errors if for some reason an output file wasn't generated while running script 1
-model_names = [f for f in listdir(path_out + 'growth_no-growth/') if isfile(join(path_out + 'growth_no-growth/', f))]
+model_names = [f for f in listdir(path + 'growth_no-growth/') if isfile(join(path + 'growth_no-growth/', f))]
 model_names = [os.path.splitext(f)[0] for f in model_names]
 
 final_compilation_table_growth = pd.DataFrame()
-final_compilation_table_growth_rate = pd.DataFrame()
-final_compilation_table_genome_growth = pd.DataFrame()
 
 for model in model_names:
     # model results are added to the yes/no growth compilation table
-    microbe_growth = pd.read_csv(path_out + 'growth_no-growth/' + model + '.csv', index_col=[0])
+    microbe_growth = pd.read_csv(path + 'growth_no-growth/' + model + '.csv', index_col=[0])
     microbe_growth = microbe_growth.transpose()
     final_compilation_table_growth = pd.concat([final_compilation_table_growth, microbe_growth])
 
 # Growth compilation table
-final_compilation_table_growth.to_csv(path_out + 'compilation/growth_compilation.csv')
+final_compilation_table_growth.to_csv(path + 'compilation/growth_compilation.csv')
